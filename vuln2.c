8382db8c892f629f59af0d41ea27276d1b7790bc
Jonas <71136545+Jonas-Zelinka@users.noreply.github.com>
2020-09-11 19:16:07 +0200

    Update vuln2.c

--- vuln2.c
+++ vuln2.c
@@ -1,4 +1,6 @@
 // SIMPLE Buffer Overflow vuln
+// echo 0 > /proc/sys/kernel/randomize_va_space 
+// this command set the ALSR off...
 // compile: gcc -g -z execstack -fno-stack-protector -no-pie vuln2.c -o vuln2
 #include <stdio.h>
 #include <string.h>
