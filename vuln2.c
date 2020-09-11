3e23383b4f47f3a2adf66585b079a3478b0ff38d
Jonas <71136545+Jonas-Zelinka@users.noreply.github.com>
2020-09-11 19:09:08 +0200

    Update vuln2.c

--- vuln2.c
+++ vuln2.c
@@ -1,3 +1,5 @@
+// SIMPLE Buffer Overflow vuln
+// compile: gcc -g -z execstack -fno-stack-protector -no-pie vuln2.c -o vuln2
 #include <stdio.h>
 #include <string.h>
 int main (int argc, char* argv[])
